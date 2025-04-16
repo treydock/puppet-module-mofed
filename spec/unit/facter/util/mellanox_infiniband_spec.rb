6f867a9c4c3984d66675bbe59b63e04841fea21f
Trey Dockendorf <tdockendorf@osc.edu>
2025-04-16 19:29:50 -0400

    Fix IB fact to not detect Mellanox Ethernet cards

--- spec/unit/facter/util/mellanox_infiniband_spec.rb
+++ spec/unit/facter/util/mellanox_infiniband_spec.rb
@@ -10,7 +10,7 @@ describe Facter::Util::MellanoxInfiniband do
 
   describe 'lspci' do
     it 'returns output' do
-      allow(Facter::Util::Resolution).to receive(:exec).with('lspci -n 2>/dev/null').and_return('foo')
+      allow(Facter::Util::Resolution).to receive(:exec).with('lspci 2>/dev/null').and_return('foo')
       expect(described_class.lspci).to eq('foo')
     end
   end
@@ -19,18 +19,18 @@ describe Facter::Util::MellanoxInfiniband do
     it 'returns count' do
       allow(Facter::Util::Resolution).to receive(:which).with('lspci').and_return('/usr/bin/lspci')
       allow(described_class).to receive(:lspci).and_return(my_fixture_read('mellanox_lspci_1'))
+      expect(described_class.count_ib_devices).to eq(2)
+    end
+
+    it 'returns count for ConnectX-3' do
+      allow(Facter::Util::Resolution).to receive(:which).with('lspci').and_return('/usr/bin/lspci')
+      allow(described_class).to receive(:lspci).and_return(my_fixture_read('mellanox_lspci_2'))
       expect(described_class.count_ib_devices).to eq(1)
     end
 
     it 'returns 0 count for noib' do
       allow(Facter::Util::Resolution).to receive(:which).with('lspci').and_return('/usr/bin/lspci')
       allow(described_class).to receive(:lspci).and_return(my_fixture_read('noib_lspci_1'))
-      expect(described_class.count_ib_devices).to eq(0)
-    end
-
-    it 'returns 0 count for qlogic' do
-      allow(Facter::Util::Resolution).to receive(:which).with('lspci').and_return('/usr/bin/lspci')
-      allow(described_class).to receive(:lspci).and_return(my_fixture_read('qlogic_lspci_1'))
       expect(described_class.count_ib_devices).to eq(0)
     end
 
